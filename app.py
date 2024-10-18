3ec01fbff8f1228f6abfd9a9b45ab64307396adc
aniketroy9172014 <aniketroy9172014@gmail.com>
2024-10-18 17:52:33 +0530

    Prepare project for deployment

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def predict():
 
     return render_template('result.html', prediction=formatted_prediction)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(debug=True)
